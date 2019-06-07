03e8b064e8dbb454ca27f5e0c62bb658cb05ed46
Courtney Couch <courtney@muut.com>
2019-06-07 11:43:20 -0700

    gc tuning
    Making gc collection more aggressive (dropping heap occupancy percent to 35, increasing pause)

--- src/scripts/elasticsearch-install.sh
+++ src/scripts/elasticsearch-install.sh
@@ -1150,11 +1150,14 @@ configure_elasticsearch()
     sed -i "s/-XX:CMSIniti/#-XX:CMSIniti/g" /etc/elasticsearch/jvm.options
     sed -i "s/-XX:+UseCMSI/#-XX:+UseCMSI/g" /etc/elasticsearch/jvm.options
     sed -i "s/# 10-:-XX/10-:-XX/g" /etc/elasticsearch/jvm.options
+    sed -i "s/InitiatingHeapOccupancyPercent=75/InitiatingHeapOccupancyPercent=35/g" /etc/elasticsearch/jvm.options
 
 cat >>/etc/elasticsearch/jvm.options <<EOL
 
--XX:MaxGCPauseMillis=400 
+-XX:MaxGCPauseMillis=500 
 -XX:G1HeapRegionSize=16m
+-XX:GCPauseIntervalMillis=1000
+-XX:+UseCondCardMark
 -XX:-PrintCommandLineFlags
 EOL
 }
